d60380ccd7b1bdfe1d28dd09a67a2cc90e1f4c59
Jakub Jucha <kuba.jucha.24@gmail.com>
2025-12-24 15:31:05 +0100

    multiplier module

---
+++ src/multiplier.sv
@@ -0,0 +1,24 @@
+module multiplier #(
+  parameter NB = 32,
+  parameter NB_FRAC = 8
+) (
+  input signed [NB-1:0] i_DATA_A,
+  input signed [NB-1:0] i_DATA_B,
+
+  output [NB-1:0] o_RESULT,
+  output o_OVFL
+);
+
+  reg signed [(2*NB)-1:0] r_full_res;
+  reg r_ovfl;
+
+  always_comb begin
+    r_full_res = i_DATA_A * i_DATA_B;
+    o_RESULT = r_full_res[NB - 1 + NB_FRAC : NB_FRAC];
+    if(i_DATA_A != 0 && i_DATA_B != 0)
+      o_OVFL = (r_full_res[(2*NB) - 1 : NB + NB_FRAC] != {(NB - NB_FRAC){r_full_res[NB + NB_FRAC - 1]}});
+    else
+      o_OVFL = 0; 
+  end
+
+endmodule
